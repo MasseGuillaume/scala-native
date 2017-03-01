3a46df121cddaa2d7a90c479bd5fce53b5dd2ee9
remi <remi.coudert@epfl.ch>
2017-03-01 19:43:31 +0100

    some more functions

--- nativelib/src/main/scala/scala/scalanative/posix/fcntl.scala
+++ nativelib/src/main/scala/scala/scalanative/posix/fcntl.scala
@@ -5,6 +5,7 @@ import scala.scalanative.native._
 /**
   * Created by remi on 28/02/17.
   */
+@extern
 object fcntl {
 
 
@@ -20,12 +21,24 @@ object fcntl {
   def AT_SYMLINK_FOLLOW: CInt = extern
   @name("scalanative_at_no_automount")
   def AT_NO_AUTOMOUNT: CInt = extern
-  @name("scalanative_empty_path")
+  @name("scalanative_at_empty_path")
   def AT_EMPTY_PATH: CInt = extern
+  @name("scalanative_at_fdcwd")
+  def AT_FDCWD = extern
   @name("scalanative_o_cloexec")
   def O_CLOEXEC: CInt = extern
   @name("scalanative_o_direct")
   def O_DIRECT: CInt = extern
+  @name("scalanative_o_rdonly")
+  def O_RDONLY: CInt = extern
+  @name("scalanative_o_wronly")
+  def O_WRONLY: CInt = extern
+  @name("scalanative_o_rdwr")
+  def O_RDWR: CInt = extern
+  @name("scalanative_o_creat")
+  def O_CREAT: CInt = extern
+  @name("scalanative_o_excl")
+  def O_EXCL: CInt = extern
   @name("scalanative_o_nonblock")
   def O_NONBLOCK: CInt = extern
 
